8187c13ebb9b47f7cd8cc052476b75362c9f163b
rofaidaessa <rofaidaessa6@gmail.com>
2026-01-06 20:51:14 +0200

    feat: Refactor API routes for better organization and clarity

--- routes/api.php
+++ routes/api.php
@@ -16,25 +16,28 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FreeTranslationController;
 use App\Http\Controllers\Auth\PasswordResetController;
 use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\FreeTransController;
-
-
 
 /* ================================
  | AUTH
  ================================= */
- Route::prefix('public')->group(function () {
-
 Route::post('/admin/login', [AuthController::class, 'login']);
 
 Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLink']);
 Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
 
+
 /* ================================
- | PARTNERS
+ | PARTNERS (Public)
  ================================= */
 Route::get('/partners', [PartnerController::class, 'publicIndex']);
 
+/* لو partner بيضرب /partner بدل /partners */
+Route::get('/partner', [PartnerController::class, 'publicIndex']);
+
+
+/* ================================
+ | PARTNERS (Admin)
+ ================================= */
 Route::prefix('admin')->group(function () {
     Route::get('/partners', [PartnerController::class, 'index']);
     Route::get('/partners/{id}', [PartnerController::class, 'show']);
@@ -47,54 +50,68 @@ Route::prefix('admin')->group(function () {
 /* ================================
  | SERVICES (Admin)
  ================================= */
-Route::get('/services', [ServiceController::class, 'index']);
-Route::post('/services', [ServiceController::class, 'store']);
-Route::get('/services/{id}', [ServiceController::class, 'show']);
-Route::post('/services/{id}', [ServiceController::class, 'update']);
-Route::delete('/services/{id}', [ServiceController::class, 'destroy']);
-
-Route::get('public/services', [ServiceController::class, 'index']);
+Route::prefix('admin')->group(function () {
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::post('/services', [ServiceController::class, 'store']);
+    Route::get('/services/{id}', [ServiceController::class, 'show']);
+    Route::post('/services/{id}', [ServiceController::class, 'update']);
+    Route::delete('/services/{id}', [ServiceController::class, 'destroy']);
+});
 
 
 /* ================================
  | LANGUAGES (Admin)
  ================================= */
-Route::get('/languages', [LanguageController::class, 'index']);
-Route::get('/languages/{id}', [LanguageController::class, 'show']);
-Route::post('/languages', [LanguageController::class, 'store']);
-Route::post('/languages/{id}', [LanguageController::class, 'update']);
-Route::delete('/languages/{id}', [LanguageController::class, 'destroy']);
+Route::prefix('admin')->group(function () {
+    Route::get('/languages', [LanguageController::class, 'index']);
+    Route::get('/languages/{id}', [LanguageController::class, 'show']);
+    Route::post('/languages', [LanguageController::class, 'store']);
+    Route::post('/languages/{id}', [LanguageController::class, 'update']);
+    Route::delete('/languages/{id}', [LanguageController::class, 'destroy']);
+});
+
 
 /* ================================
  | CONTACT
  ================================= */
 Route::post('/contact', [ContactController::class, 'store']);
-Route::get('/contact-messages', [ContactController::class, 'index']);
-Route::get('/contact-messages/{id}', [ContactController::class, 'show']);
-Route::post('/contact-messages/{id}', [ContactController::class, 'update']);
-Route::delete('/contact-messages/{id}', [ContactController::class, 'destroy']);
-Route::post('/contact-messages/{id}/read-status', [ContactController::class, 'updateReadStatus']);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/contact-messages', [ContactController::class, 'index']);
+    Route::get('/contact-messages/{id}', [ContactController::class, 'show']);
+    Route::post('/contact-messages/{id}', [ContactController::class, 'update']);
+    Route::delete('/contact-messages/{id}', [ContactController::class, 'destroy']);
+    Route::post('/contact-messages/{id}/read-status', [ContactController::class, 'updateReadStatus']);
+});
 
 
+/* ================================
+ | ARTICLES
+ ================================= */
 /* Dashboard */
-Route::get('/articles', [ArticleController::class, 'index']);
-Route::post('/articles', [ArticleController::class, 'store']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
-Route::put('/articles/{id}', [ArticleController::class, 'update']);
-Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+Route::prefix('admin')->group(function () {
+    Route::get('/articles', [ArticleController::class, 'index']);
+    Route::post('/articles', [ArticleController::class, 'store']);
+    Route::get('/articles/{id}', [ArticleController::class, 'show']);
+    Route::put('/articles/{id}', [ArticleController::class, 'update']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+});
 
 /* Website */
-Route::get('/public/articles', [ArticleController::class, 'publicIndex']);
-Route::get('/public/articles/{slug}', [ArticleController::class, 'publicShow']);
+Route::get('/articles', [ArticleController::class, 'publicIndex']);
+Route::get('/articles/{slug}', [ArticleController::class, 'publicShow']);
+
 
 /* ================================
  | FREE TRANSLATION
  ================================= */
-Route::post('/free-translation', [FreeTransController::class, 'send']);
- Route::post('/free-translation-request', [FreeTranslationController::class, 'store']);
-Route::get('/free-translation-requests', [FreeTranslationController::class, 'index']);
-Route::get('/free-translation-requests/{id}', [FreeTranslationController::class, 'show']);
-Route::post('/free-translation-requests/{id}', [FreeTranslationController::class, 'updateStatus']);
-Route::delete('/free-translation-requests/{id}', [FreeTranslationController::class, 'destroy']);
+/* بدل FreeTransController اللي مش موجود */
+Route::post('/free-translation', [FreeTranslationController::class, 'store']);
 
+Route::prefix('admin')->group(function () {
+    Route::post('/free-translation-request', [FreeTranslationController::class, 'store']);
+    Route::get('/free-translation-requests', [FreeTranslationController::class, 'index']);
+    Route::get('/free-translation-requests/{id}', [FreeTranslationController::class, 'show']);
+    Route::post('/free-translation-requests/{id}', [FreeTranslationController::class, 'updateStatus']);
+    Route::delete('/free-translation-requests/{id}', [FreeTranslationController::class, 'destroy']);
 });
